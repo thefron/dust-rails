d619a5a1844e0dccf82e9e7564f771f7cfd822f8
Linas Juškevičius <linas@kaukas.net>
2012-10-24 18:18:22 +0300

    Support rake asset:precompile in Rails 3.1+.
    Note: not tested on 3.0.

--- lib/dust-rails/rails/engine.rb
+++ lib/dust-rails/rails/engine.rb
@@ -3,7 +3,7 @@ require 'rails/engine'
 module Dust
   module Rails
     class Engine < ::Rails::Engine
-      initializer :register_dustjs do |app|
+      initializer :register_dustjs, :group => :all do |app|
         app.assets.register_engine '.dust', DustTemplate
       end
     end
