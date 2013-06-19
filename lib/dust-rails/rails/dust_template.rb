844f7a93a270230df16916dc6e2d2f6618dd271e
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2013-06-19 19:28:22 -0300

    bugfix dust.compile

--- lib/dust-rails/rails/dust_template.rb
+++ lib/dust-rails/rails/dust_template.rb
@@ -5,16 +5,10 @@ module Dust
   module Rails
 
     module Source
-      def self.path
-        @path ||= File.expand_path('../../../../vendor/dustjs/lib/dust.js', __FILE__)
-      end
-
-      def self.contents
-        @contents ||= File.read(path)
-      end
-
       def self.context
-        @context ||= ExecJS.compile(contents)
+        path = File.expand_path('../../../../vendor/dustjs/lib/dust.js', __FILE__)
+        contents = File.read(path)
+        ExecJS.compile(contents)
       end
 
     end
@@ -31,8 +25,7 @@ module Dust
       def evaluate(scope, locals, &block)
         template_root = Dust.config.template_root
         template_name = file.split(template_root).last.split('.',2).first
-        dust_source = "dust.loadSource(dust.compile(#{data},#{template_name}));"
-        Source.context.call(dust_source)
+        Source.context.call("dust.compile", data, template_name)
       end
     end
   end
